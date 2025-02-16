cef53ed1cd51d7ba202850208a7f38443d6e9ace
DinPX <dinpixels@proton.me>
2025-02-16 17:51:39 +0800

    Updated home, Linux Gaming, and blog pages, added images for Linux Gaming sections, etc.

--- src/js/themes.js
+++ src/js/themes.js
@@ -18,8 +18,12 @@ for (let i of switches) {
 function setTheme(theme) {
     if (theme == 'xp') {
         document.getElementById('switcher-id').href = 'src/css/themes/homepage/xp.css';
+        document.getElementById('chattable').src = document.getElementById('chattable').src
+        chattable.initialize({stylesheet : "src/css/themes/chattable/xp.css"});
     } else if (theme == 'sakura') {
         document.getElementById('switcher-id').href = 'src/css/themes/homepage/sakura.css';
+        document.getElementById('chattable').src = document.getElementById('chattable').src
+        chattable.initialize({stylesheet : "src/css/themes/chattable/sakura.css"});
     }
     localStorage.setItem('style', theme);
 }
